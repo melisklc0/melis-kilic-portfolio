206c6950446bcd30143449bdbda3f539565c625a
Melis Kılıç <klmelis@hotmail.com>
2026-05-23 23:44:58 +0300

    feat: update CV URLs in profile data to reflect new file names

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -6,7 +6,7 @@ export const profile = {
   github: 'https://github.com/melisklc0',
   linkedin: 'https://linkedin.com/in/melisklc0',
   cv_url: {
-    en: `${import.meta.env.BASE_URL}cv/melis-kilic-cv-EN.pdf`,
-    tr: `${import.meta.env.BASE_URL}cv/melis-kilic-cv-TR.pdf`
+    en: `${import.meta.env.BASE_URL}cv/Melis_Kilic_EN_CV.pdf`,
+    tr: `${import.meta.env.BASE_URL}cv/Melis_Kilic_TR_CV.pdf`
   },
 };
